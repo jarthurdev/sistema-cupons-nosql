2a790a44383d95d24b7ae7ba6b71df783ef48a4a
ArthurHDEV <arthurhap.pro@gmail.com>
2025-11-28 15:06:58 -0300

    Criação do modelo Coupon e o método para criação no DynamoDB

---
+++ src/models/coupon.js
@@ -0,0 +1,58 @@
+// src/models/Coupon.js
+import { PutCommand } from "@aws-sdk/lib-dynamodb";
+import { client } from "../config/db.js";
+
+export class CouponModel {
+  static TABLE_NAME = "Coupons";
+
+  /**
+   * Cria um novo cupom no banco.
+   * @param {Object} data - Dados do cupom
+   */
+  static async create(data) {
+    const now = new Date();
+    
+    // Define estrutura padrão
+    const item = {
+      couponId: data.code.toUpperCase(), // PK é o código em maiúsculo
+      description: data.description,
+      status: "active",
+      
+      // Regras encapsuladas
+      rules: {
+        type: data.type || "fixed", 
+        value: data.value,
+        minOrderValue: data.minOrderValue || 0,
+      },
+
+      // Controle de Estoque
+      quota: {
+        max: data.maxUsage || 100,
+        used: 0 // Começa com 0
+      },
+
+      createdAt: now.toISOString(),
+      // Calcula TTL (expira em X dias)
+      expiresAt: Math.floor(now.getTime() / 1000) + (data.validityHours * 3600)
+    };
+
+    // Salva no DynamoDB
+    const command = new PutCommand({
+      TableName: this.TABLE_NAME,
+      Item: item,
+      // Garante que não sobrescreve um cupom que já existe com esse código
+      ConditionExpression: "attribute_not_exists(couponId)"
+    });
+
+    try {
+      await client.send(command);
+      console.log(`✅ Cupom ${item.couponId} criado!`);
+      return item;
+    } catch (error) {
+      if (error.name === "ConditionalCheckFailedException") {
+        throw new Error("Cupom com este código já existe.");
+      }
+      throw error;
+    }
+  }
+}
